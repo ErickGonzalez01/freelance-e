17687447b2839c05ea0d5d8e525dbc31ba2762d5
erickgonzalez01 <erickjoelg@gmail.com>
2025-03-20 19:50:53 -0600

    20-03-2025 07:50 pm NavBar para moviles

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 import './style.css'
+
+let aside = document.getElementById('aside');
+let btnMenu = document.getElementById('btn-menu');
+
+let hidden = "hidden";
+let opacity0 = "opacity-0";
+let opacity100 = "opacity-100";
+
+btnMenu.addEventListener('click', () => {
+    if (aside.classList.contains(hidden)) {
+        aside.classList.remove(hidden);
+        aside.classList.remove(opacity0);
+        aside.classList.add(opacity100);
+    } else {
+        aside.classList.add(opacity0);
+        setTimeout(() => {
+            aside.classList.add(hidden);
+        }, 700);
+    }
+});
